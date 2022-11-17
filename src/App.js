b22d6590033fbcee2f545d41fbc9172b650d97a4
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-17 19:33:24 +0100

    created routes for the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
 
+
 import Header from "./components/Header/Header"
+import Women from './pages/Home';
+import Men from './pages/Men/Men'
+import Kids from './pages/Kids/Kids'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path='/' element={<Women/>} />
+        <Route path='/men' element={<Men />} />
+        <Route path='/kids' element={<Kids/>} />
+      </Routes>
     </div>
   );
 }
