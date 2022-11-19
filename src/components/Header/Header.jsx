d58af070f72eb0997577ba468e3b01021917b898
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-19 20:17:07 +0100

    added basic acction fields in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import Navbar from '../Navbar/Navbar'
 import styles from "./Header.module.css"
 
 import logo from "../../assets/logo.png"
+import cartImg from "../../assets/cart.svg"
 
 export default class Header extends Component {
   render() {
@@ -15,7 +16,14 @@ export default class Header extends Component {
         <div className={styles.logoContainer}>
           <img src={logo} alt="Logo" />
         </div>
-        <div>Actions</div>
+        <div>
+          <select name="curency" id="curency">
+            <option value="dollar">$ USD</option>
+            <option value="dollar">$ EUR</option>
+            <option value="dollar">$ JPY</option>
+          </select>
+          <img src={cartImg} alt="cart" />
+        </div>
       </header>
     )
   }
